073089505679bb5ffc03a83c96893da3f4f5279d
Pierre-ZACHARY <87607183+Pierre-ZACHARY@users.noreply.github.com>
2022-09-07 23:46:32 +0200

    STOP SPAM

--- bot/main.py
+++ bot/main.py
@@ -8,7 +8,7 @@ from typing import Union
 
 import discord
 import websockets as websockets
-from discord import CategoryChannel
+from discord import CategoryChannel, TextChannel, Message
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="!")
@@ -17,6 +17,7 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 websites = {}
 uid_channels = {}
 keyDict = {}
+websockets_todo_setname = {}
 websites["CHANNEL_ID"] = os.getenv("CHANNEL_ID")
 
 
@@ -68,7 +69,8 @@ async def on_message(message: discord.message):
 
 
 @bot.event
-async def on_typing(channel: discord.abc.Messageable, user: Union[discord.User, discord.Member], when: datetime.datetime):
+async def on_typing(channel: discord.abc.Messageable, user: Union[discord.User, discord.Member],
+                    when: datetime.datetime):
     if user.id != bot.user.id:
         channel_id = channel.id
         if channel_id in connected:
@@ -87,28 +89,35 @@ async def consumer_handler(websocket, path):
                 continue
             if str(websocket) not in keyDict:
                 keyDict[str(websocket)] = message["key"]
-            if message["key"] not in uid_channels:
-                channel = bot.get_channel(int(categorie_id))
-                textchannel = await channel.create_text_channel(str(uuid.uuid1()))
-                uid_channels[message["key"]] = textchannel
-                connected[textchannel.id] = websocket
-            else:
-                textchannel = uid_channels[message["key"]]
             if "set_name" in message:
-                if textchannel.name != message["set_name"]:
-                    try:
-                        await asyncio.wait_for(textchannel.edit(name=message["set_name"]), timeout=1)
-                    except asyncio.TimeoutError:
-                        await textchannel.send("Tried to change channel name to : "+message["set_name"]+" ( API cap exceeded 2edits/10min )")
-                        print("Api cap exceeded")
+                websockets_todo_setname[str(websocket)] = message["set_name"]
             if "content" in message and message["content"] != "":
+                if message["key"] not in uid_channels:
+                    channel = bot.get_channel(int(categorie_id))
+                    textchannel = await channel.create_text_channel(str(uuid.uuid1()))
+                    uid_channels[message["key"]] = textchannel
+                    connected[textchannel.id] = websocket
+                else:
+                    textchannel = uid_channels[message["key"]]
+                if websockets_todo_setname[str(websocket)] is not None:
+                    if textchannel.name != websockets_todo_setname[str(websocket)]:
+                        try:
+                            await asyncio.wait_for(
+                                textchannel.edit(name=websockets_todo_setname[str(websocket)], timeout=1))
+                        except asyncio.TimeoutError:
+                            await textchannel.send("Tried to change channel name to : " + websockets_todo_setname[
+                                str(websocket)] + " ( API cap exceeded 2edits/10min )")
+                    websockets_todo_setname[str(websocket)] = None
+
                 await textchannel.send(message["content"])
     except websockets.ConnectionClosed as e:
         if str(websocket) in keyDict:
             key = keyDict[str(websocket)]
-            textchannel = uid_channels[key]
-            print(textchannel)
-            await textchannel.send("Info : Websocket closed.")
+            if key in uid_channels:
+                textchannel: TextChannel = uid_channels[key]
+                last_msg: Message = textchannel.last_message
+                if last_msg.content != "Info : Websocket closed.":
+                    await textchannel.send("Info : Websocket closed.")
 
 
 if __name__ == "__main__":
@@ -117,10 +126,9 @@ if __name__ == "__main__":
         host="0.0.0.0",
         port=int(os.environ["PORT"]),
     )
-    print("listening on port "+str(int(os.environ["PORT"])))
+    print("listening on port " + str(int(os.environ["PORT"])))
     loop = asyncio.new_event_loop()
     stop = loop.create_future()
     loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
     loop.run_until_complete(asyncio.wait([server, stop], return_when=asyncio.FIRST_COMPLETED))
     bot.run(TOKEN)
-
